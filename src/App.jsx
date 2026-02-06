9b65beb0ab4e82ce0d5650f20a981e1218cdb9a4
godwin <your_mail>
2026-02-06 23:50:12 +0100

    Moved over the tracking page to the react codebase and also routed it.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
 import Orders from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 		<Route path='/' element={<HomePage />} />
 		<Route path='checkout' element={<CheckoutPage />} />
 		<Route path='orders' element={<Orders />} />
+		<Route path='tracking' element={<TrackingPage />} />
 	</Routes>
   )
 }
